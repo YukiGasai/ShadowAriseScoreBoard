971994acbe61e69dfa72fdd1f3d9332282897af9
YukiGasai <r.lindede@googlemail.com>
2021-12-28 23:52:54 +0100

    Added routes

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
+const {
+	uniqueNamesGenerator,
+	adjectives,
+	colors,
+	animals,
+} = require('unique-names-generator');
+
+const randomName = seed => {
+	return uniqueNamesGenerator({
+		dictionaries: [adjectives, colors, animals],
+		seed,
+	});
+};
+
+const cleanScores = scores => {
+	return scores.map(score => {
+		const numberIp = parseInt(score.ip.replace(/\D/g, ''), 10);
+		return { ...score, ip: randomName(numberIp) };
+	});
+};
+
 mongoose.connect(process.env.DB_URL, () => {
 	console.log('Connected to db');
 });
@@ -16,15 +37,69 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-app.post('/api/view', async (req, res) => {
-	if (req.body.password === 'Gronbowski') {
-		const scores = await Score.find();
-		res.status(200).json(scores);
+/**
+ * List all highscores but group by ip
+ */
+app.get('/api/view', async (req, res) => {
+	const sort = req.query.sort || 'gameTime';
+	const sortOrder = req.query.sortOrder || 1;
+	const showGrouped = req.query.grouped || 'true';
+	const scores = await Score.find({})
+		.sort([[sort, sortOrder]])
+		.lean();
+
+	const grouped = [];
+	console.log(showGrouped);
+	if (showGrouped == 'true') {
+		scores.forEach(score => {
+			if (!grouped.some(group => group.ip === score.ip)) {
+				grouped.push(score);
+			}
+		});
+		res.status(200).json(cleanScores(grouped));
 	} else {
-		res.status(200).json({ msg: 'hi' });
+		res.status(200).json(cleanScores(scores));
 	}
 });
 
+/**
+ * Get Player Name
+ */
+app.get('/api/name', async (req, res) => {
+	const ipAddr =
+		req.headers['x-forwarded-for'] || req.socket.remoteAddress || '187.361.0.1';
+
+	const numberIp = parseInt(ipAddr.replace(/\D/g, ''), 10);
+
+	res.status(200).json({ name: randomName(numberIp) });
+});
+
+/**
+ * Get own Scores
+ */
+app.get('/api/me', async (req, res) => {
+	let result = {};
+	const ipAddr =
+		req.headers['x-forwarded-for'] || req.socket.remoteAddress || '187.361.0.1';
+
+	const sort = req.query.sort || 'gameTime';
+	const sortOrder = req.query.sortOrder || 1;
+	const scores = await Score.find({ ip: ipAddr })
+		.sort([[sort, sortOrder]])
+		.lean();
+
+	const numberIp = parseInt(ipAddr.replace(/\D/g, ''), 10);
+	result.ip = randomName(numberIp);
+	result.amount = scores.length;
+	result.scores = cleanScores(scores);
+
+	res.send(result);
+});
+
+/**
+ * Add new Score to the list doesn't have to be highscore
+ * Triggered by ingame unity Hub
+ */
 app.post('/api/add', async (req, res) => {
 	if (req.body.password === 'Gronbowski') {
 		const ipAddr =
@@ -37,15 +112,6 @@ app.post('/api/add', async (req, res) => {
 			deathCounter: req.body.deathCounter,
 			gameTime: req.body.gameTime,
 		});
-		const savedScore = await newSore.save();
-		res.status(200).json(savedScore);
-	} else {
-		res.status(401).json({ msg: 'cheater' });
-	}
-});
-
-app.post('/api/position', async (req, res) => {
-	if (req.body.password === 'Gronbowski') {
 		const savedScore = await newSore.save();
 		res.status(200).json(savedScore);
 	} else {
