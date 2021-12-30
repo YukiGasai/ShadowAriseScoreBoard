7b143fa890bb4b7d391a58d0f5721f4c3d30baab
YukiGasai <r.lindede@googlemail.com>
2021-12-30 18:48:12 +0100

    Fixed name to uniq id

--- helper/randomName.js
+++ helper/randomName.js
@@ -6,11 +6,31 @@ const {
 } = require('unique-names-generator');
 
 const randomName = seed => {
-	return uniqueNamesGenerator({
+	const og = seed;
+	//Only Hex values stay
+	seed = seed.replace(/[^A-Fa-f0-9]/g, '');
+
+	//Cut id down to fit into seed size
+	if (seed.length > 19) {
+		seed = seed.substring(0, 19);
+	}
+
+	//Turn hex to int
+	seed = parseInt(seed, 16);
+
+	const name = uniqueNamesGenerator({
 		dictionaries: [adjectives, colors, animals],
 		seed: seed,
 		length: 2,
 	});
+
+	console.log({
+		og,
+		seed,
+		name,
+	});
+
+	return name;
 };
 
 module.exports.randomName = randomName;
