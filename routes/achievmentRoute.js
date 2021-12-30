9c0007f6f55eb907f45173d43f77c8605c9f18e5
YukiGasai <r.lindede@googlemail.com>
2021-12-30 13:48:13 +0100

    Added SucessRate on player

--- routes/achievmentRoute.js
+++ routes/achievmentRoute.js
@@ -3,6 +3,24 @@ const AchievmentTemplateModel = require('../models/achievmentTemplateModel');
 const Achievment = require('../models/achievmentModel');
 const { randomName } = require('../helper/randomName');
 
+const getStats = async index => {
+	let count = await Achievment.find({ isFinished: true }).populate('template');
+
+	count = count.filter(achievment => achievment.template.index == 10);
+
+	count = count.length;
+
+	let total = await Achievment.find({}).populate('template');
+
+	total = total.filter(achievment => achievment.template.index == index);
+
+	total = total.length;
+
+	if (total == 0) return 0;
+
+	return (count / total) * 100;
+};
+
 router.post('/all', async (req, res) => {
 	if (!req.body.password || req.body.password != process.env.PASS)
 		return res.status(400).json({ err: true, msg: 'Missing data' });
@@ -27,13 +45,47 @@ router.post('/player', async (req, res) => {
 	)
 		return res.status(400).json({ err: true, msg: 'Missing data' });
 
-	const filter = req.body.index
-		? { index: req.body.index, name: randomName(req.body.ip) }
-		: { name: randomName(req.body.ip) };
+	const allTemplates = await AchievmentTemplateModel.find({})
+		.lean()
+		.sort([['index', -1]]);
 
-	const foundAchievments = await Achievment.find(filter)
+	const foundAchievments = await Achievment.find({
+		name: randomName(req.body.ip),
+	})
 		.populate('template')
-		.sort([['createdAt', -1]]);
+		.lean()
+		.sort([['index', -1]]);
+
+	allTemplates.forEach(template => {
+		const isFound = foundAchievments.some(
+			achievment => achievment.template.index == template.index
+		);
+
+		if (!isFound) {
+			foundAchievments.push({
+				_id: '',
+				name: '',
+				template: template,
+				process: 0,
+				isFinished: false,
+				createdAt: '',
+				updatedAt: '',
+				__v: 0,
+			});
+		}
+	});
+
+	foundAchievments.sort(function (a, b) {
+		return a.template.index - b.template.index;
+	});
+
+	for (let i = 0; i < foundAchievments.length; i++) {
+		const successRate = await getStats(foundAchievments[i].template.index);
+		foundAchievments[i].template = {
+			...foundAchievments[i].template,
+			successRate,
+		};
+	}
 
 	return res.status(200).json(foundAchievments);
 });
